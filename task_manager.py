79ff338398b65576d607656e1f483e356933aab8
marvus-creator <malvyn304@gmail.com>
2025-12-03 11:06:00 +0200

    refactor: optimize task sorting logic part 1

---
+++ task_manager.py
@@ -0,0 +1,9 @@
+class Task:
+    def __init__(self, name, priority):
+        self.name = name
+        self.priority = priority
+
+tasks = [Task("Learn Git", "High"), Task("Build Portfolio", "Medium")]
+for t in tasks:
+    print(f"Task: {t.name} | Priority: {t.priority}")
+# Update v1.1 - Refactoring code
